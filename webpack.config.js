d11788b289e7226e95d4cf0e0bedbf114307262c
Tatyana Fomina <tayna-anita@mail.ru>
2020-01-30 18:49:54 +0300

    Добавила css, bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
         test: /\.(js|jsx)?$/,
         exclude: /(node_modules)/,
         use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
