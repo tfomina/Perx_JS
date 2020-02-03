9d9c1bb5ca66ccda02c19c34f67ee695115742fa
Tatyana Fomina <tayna-anita@mail.ru>
2020-02-03 19:32:34 +0300

    Мелкие правки

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import Spinner from "react-bootstrap/Spinner";
 export const Loader = () => (
   <div className="d-flex justify-content-center">
     <Spinner animation="border" role="status" variant="primary">
-      <span className="sr-only">Loading...</span>
+      <span className="sr-only">Загрузка...</span>
     </Spinner>
   </div>
 );
